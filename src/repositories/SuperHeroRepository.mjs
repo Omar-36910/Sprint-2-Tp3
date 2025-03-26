2a0024253577557c2f7b6451f0d0596885e63f69
Omar-36910 <omarbarros@gmail.com>
2025-03-26 17:07:34 -0300

    actualizando el endpoint mayores de 30 con planeta origen y poderes

--- src/repositories/SuperHeroRepository.mjs
+++ src/repositories/SuperHeroRepository.mjs
@@ -19,7 +19,8 @@ class SuperHeroRepository extends IRepository {
     };
 
     async obtenerMayoresDe30() {
-        return await SuperHero.find({ edad: { $gt: 30 } });
+        return await SuperHero.find({ edad: { $gt: 30 }, planetaOrigen: 'Tierra', $expr: { $gte: [{ $size: "$poderes" }, 2] }  });
+       
     };
 }
 
